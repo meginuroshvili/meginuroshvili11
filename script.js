cc774b986f8ecb75456ddc17d37aad7b4b9362d4
meginuroshvili <mnuroshvili@gmail.com>
2021-10-26 21:56:21 +0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let user = {
     lastname: 'smith',
     age: 25,
     studentstatus: 'active'
-};
+}
 
 console.log(user.studentstatus)
 
@@ -20,4 +20,41 @@ let item = 0;
 while (item<cars.length) {
     console.log(cars[item]);
     item++;
+}
+
+// example 3 
+
+let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89]
+
+for (x=0; x<numbers.length; x++ ) {
+if(numbers[x]>5)
+
+console.log(numbers[x])
+}
+
+// example 4 
+
+let array = ["html", "css", "bootstrap", "javascript","python"]
+
+for (x=0; x<array.length; x++){
+if (array[x].length>5 && array[x].includes('ava'))
+    console.log(array[x])
+}
+
+// example 5 
+
+let users = {
+    name: 'giorgi',
+    age:  20,
+    studentstatus: 'active'
+}
+
+if (users.age>18 && users.studentstatus == 'active'){
+    console.log('hello')
+}
+if (users.name == 'giorgi'){
+    console.log('hello giorgi')
+}
+if (users.studentstatus == 'actve' || users.age<25) {
+    console.log('hello world')
 }
