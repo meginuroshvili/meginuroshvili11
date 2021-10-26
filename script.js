6444acf7aae7a91c79f9358d28b26b53942b0361
meginuroshvili <mnuroshvili@gmail.com>
2021-10-26 19:52:18 +0400

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,15 @@ let user = {
 console.log(user.studentstatus)
 
 // example 2
+// for cycle
 let cars = ['bmw', 'audi', 'porsche', 'ferrari', 'mercedes']
 
 for (let x=0; x<5; x++)
 
 console.log(cars[x])
+// while cycle
+let item = 0; 
+while (item<cars.length) {
+    console.log(cars[item]);
+    item++;
+}
